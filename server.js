d47e9e47705b12c63bb4a01a1fb7419fbf86356d
isaac <simstarr18@yahoo.com>
2018-07-13 10:10:00 -0700

    initial site files

--- server.js
+++ server.js
@@ -2,22 +2,47 @@ var express = require("express");
 var exphbs = require("express-handlebars");
 var bodyParser = require("body-parser");
 var logger = require("morgan");
-var mongoose = require("mongoose");
-
-var db = require("./models");
 
 var PORT = process.env.PORT || 8000;
 
 var app = express();
 
+var db = require("./models");
+
+//mongoose db configuration
+var mongoose = require("mongoose");
+var request = require("request");
+var cherrio = require("cheerio");
+app.use(logger("dev"));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use(express.static("public"));
+var databaseUri = "mongodb://localhost/scraper";
+if (process.env.MONGODB_URI) {
+  mongoose.connect(process.env.MONGODB_URI);
+} else {
+  mongoose.connect(databaseUri);
+}
+//end of mongoose configuration
+
+// //mongoose error
+// var db = mongoose.connection;
+
+// db.on('error', function (err) {
+//   console.log('mongoose error: ', err);
+// });
+// //end of mongoose error
+
+// //mongoose success message
+// db.once('open', function () {
+//   console.log('mongoose connection successful: ', err);
+// });
+// //mongoose success end
+
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
-app.use(logger("dev"));
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static("public"));
-
-mongoose.Promise = Promise;
-mongoose.connect("mongodb://localhost/scraper");
 
 require('./routes/routes.js')(app);
 
